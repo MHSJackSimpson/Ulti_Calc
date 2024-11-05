b4622a76223f69a0cb6860a00a6bc05be86e865c
MHSJackSimpson <simpsonj3018@masseyhigh.school.nz>
2024-11-06 12:01:49 +1300

    Update 01_Bulk_Calc_v2.py

--- 01_Bulk_Calc_v2.py
+++ 01_Bulk_Calc_v2.py
@@ -84,62 +84,60 @@ if want_instructions == "":
 
 
 print("program continues")
-
-domain = get_domain()
-
-print("Domain:", domain)
-
-if domain == "mass":
-    unit_list = mass_units
-elif domain == "distance":
-    unit_list = distance_units
-else:
-    unit_list = time_units
-
-from_unit = get_unit(unit_list)
-to_unit = get_unit(unit_list)
-
-print(f"\nYou chose to go from {from_unit} to {to_unit}\n")
-
-distance_dict = {
-    "mm": 1000,
-    "cm": 100,
-    "m": 1,
-    "km": .001,
-}
-
-mass_dict = {
-    "mg": 1000,
-    "g": 1,
-    "kg": 0.001,
-}
-
-
-time_dict = {
-    "sec": 86400,
-    "min": 1440,
-    "hrs": 24,
-    "days": 1,
-}
-
-amount = float(input("How much? "))
-if domain == "distance":
-    diction = distance_dict
-if domain == "mass":
-    diction = mass_dict
-elif domain == "time":
-    diction = time_dict
-
-# standard value
-multiply_by = diction[to_unit]
-standard = amount * multiply_by
-
-# desired value
-divide_by = diction[from_unit]
-answer = standard / divide_by
-
-
-print(f"\nThere is {answer} {to_unit} in {amount} {from_unit} ")
 while True:
-    keep_going = input("\nPress 'ENTER' to keep going or type 'xxx' then press enter to quit! ")
+    domain = get_domain()
+
+    print("Domain:", domain)
+
+    if domain == "mass":
+        unit_list = mass_units
+    elif domain == "distance":
+        unit_list = distance_units
+    else:
+        unit_list = time_units
+
+    from_unit = get_unit(unit_list)
+    to_unit = get_unit(unit_list)
+
+    print(f"\nYou chose to go from {from_unit} to {to_unit}\n")
+
+    distance_dict = {
+        "mm": 1000,
+        "cm": 100,
+        "m": 1,
+        "km": .001,
+    }
+
+    mass_dict = {
+        "mg": 1000,
+        "g": 1,
+        "kg": 0.001,
+    }
+
+    time_dict = {
+        "sec": 86400,
+        "min": 1440,
+        "hrs": 24,
+        "days": 1,
+    }
+
+    amount = float(input("How much? "))
+    if domain == "distance":
+        diction = distance_dict
+    if domain == "mass":
+        diction = mass_dict
+    elif domain == "time":
+        diction = time_dict
+
+    # standard value
+    multiply_by = diction[to_unit]
+    standard = amount * multiply_by
+
+    # desired value
+    divide_by = diction[from_unit]
+    answer = standard / divide_by
+
+    print(f"\nThere is {answer} {to_unit} in {amount} {from_unit} ")
+    keep_going = input("Press ENTER to keep going or type no then press enter to quit")
+    print()
     print("Thank you for using this program :)")
